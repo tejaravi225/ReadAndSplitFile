c22c0efe40d11b75e7825fad54dc5935cda48004
tejaravi225 <tejaravi225@gmail.com>
2015-11-16 20:40:26 +0530

    HelloGit file is added

---
+++ src/readandsplitfile/HelloGit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readandsplitfile;
+
+/**
+ *
+ * @author miracle
+ */
+public class HelloGit {
+    public void sample()
+    {
+    
+        System.out.println("Hello Git Sample method");
+    }
+}
